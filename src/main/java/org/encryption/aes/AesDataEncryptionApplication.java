0665adc3314949e127995e29c483c609ac9f944e
a0u007a <Amit.Upadhyay@walmart.com>
2020-05-19 22:48:22 +0530

    Initial Commit

---
+++ src/main/java/org/encryption/aes/AesDataEncryptionApplication.java
@@ -0,0 +1,13 @@
+package org.encryption.aes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesDataEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AesDataEncryptionApplication.class, args);
+	}
+
+}
